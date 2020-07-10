0213a17bfb1217941a8a8c4c48e4975d1274a808
ali-m-d <alistair_divorty@hotmail.com>
2020-07-10 10:58:38 +0000

    Set up database

--- word-frequency-comparator.sh
+++ word-frequency-comparator.sh
@@ -6,3 +6,22 @@ sudo sudo -u postgres psql
 # \q
 
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev postgresql-client-common postgresql-client libpq-dev software-properties-common
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
+
+# install webpack
+
+sudo -u postgres
+psql
+ALTER USER "ubuntu" WITH SUPERUSER;
+\q
+exit
+
+sudo su postgres
+psql
+CREATE ROLE ali_m_d WITH LOGIN PASSWORD 'ka76wr'
+CREATE DATABASE word_frequency_comparator_development OWNER ali_m_d;
+Ctrl-D
+\q
