9eb4c04c91ab14b684e7768b919371d999d6161a
ali-m-d <65289824+ali-m-d@users.noreply.github.com>
2020-07-10 11:27:08 +0100

    Initial commit

---
+++ word-frequency-comparator.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install postgresql
+sudo service postgresql start
+sudo sudo -u postgres psql
+# \password
+# \q
+
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev postgresql-client-common postgresql-client libpq-dev software-properties-common
