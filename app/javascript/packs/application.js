ea295a9728c950714f363c9d43bc95a2693727d2
ali-m-d <alistair_divorty@hotmail.com>
2020-07-12 13:07:34 +0000

    Added folder modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'bootstrap'
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
+// require("jquery")
+import 'bootstrap';
+import JQuery from 'jquery'
+window.$ = window.JQuery = JQuery;
 
 $(document).ready(function () {
    setTimeout(function() {
       $('.alert-text').fadeOut();
-   }, 2000);
+   }, 4000);
 });
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
