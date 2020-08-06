b25f7b269a2bb6d30d6447c8d458bbf13da4ff24
ali-m-d <alistair_divorty@hotmail.com>
2020-08-06 14:51:41 +0000

    Adjusted stacking of charts

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -2,14 +2,8 @@ class FoldersController < ApplicationController
   before_action :set_folder, only: [:show, :edit, :update, :destroy]
 
   # GET /folders
-  # GET /folders.json
   def index
     @folders = current_user.folders.all
-  end
-
-  # GET /folders/1
-  # GET /folders/1.json
-  def show
   end
 
   # GET /folders/new
@@ -17,20 +11,15 @@ class FoldersController < ApplicationController
     @folder = current_user.folders.new
   end
 
-  # GET /folders/1/edit
-  def edit
-  end
-
   # POST /folders
-  # POST /folders.json
   def create
     @folder = current_user.folders.new(folder_params)
 
     respond_to do |format|
       if @folder.save
         format.js
-        format.html { redirect_to folder_url, notice: 'Folder was successfully created.' }
-        format.json { render :show, status: :created, location: @folder }
+        format.html { redirect_to folder_url }
+        format.json { head :no_content }
       else
         format.js { render 'errors.js.erb' }
         # format.json { render json: @folder.errors, status: :unprocessable_entity }
@@ -38,38 +27,38 @@ class FoldersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /folders/1
-  # PATCH/PUT /folders/1.json
-  def update
-    respond_to do |format|
-      if @folder.update(folder_params)
-        format.html { redirect_to @folder, notice: 'Folder was successfully updated.' }
-        format.json { render :show, status: :ok, location: @folder }
-      else
-        format.html { render :edit }
-        format.json { render json: @folder.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /folders/1
-  # DELETE /folders/1.json
   def destroy
     @folder.destroy
     respond_to do |format|
       format.js
-      format.html { redirect_to folders_url, notice: 'Folder was successfully destroyed.' }
+      format.html { redirect_to folders_url }
       format.json { head :no_content }
     end
   end
+  
+  # def edit
+  # end
+  
+  # def show
+  # end
+  
+  # def update
+  #   respond_to do |format|
+  #     if @folder.update(folder_params)
+  #       format.html { redirect_to @folder, notice: 'Folder was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @folder }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @folder.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_folder
       @folder = Folder.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def folder_params
       params.require(:folder).permit(:name)
     end
